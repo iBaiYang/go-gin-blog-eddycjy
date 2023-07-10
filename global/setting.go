301da784d585bf9e5571fc016fa55094083d4810
dingdong <1428837023@qq.com>
2023-07-10 18:19:33 +0800

    配置管理 add

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/iBaiYang/go-gin-blog-eddycjy/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
--- pkg/errcode/errcode.go
+++ pkg/errcode/errcode.go
@@ -55,6 +55,20 @@ func (e *Error) StatusCode() int {
 	switch e.Code() {
 	case Success.Code():
 		return http.StatusOK
+	case ServerError.Code():
+		return http.StatusInternalServerError
+	case InvalidParams.Code():
+		return http.StatusBadRequest
+	case UnauthorizedAuthNotExist.Code():
+		fallthrough
+	case UnauthorizedTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenGenerate.Code():
+		fallthrough
+	case UnauthorizedTokenTimeout.Code():
+		return http.StatusUnauthorized
+	case TooManyRequests.Code():
+		return http.StatusTooManyRequests
 	}
 
 	return http.StatusInternalServerError
