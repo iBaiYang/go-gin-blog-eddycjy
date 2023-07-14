4744dc0f1a258985d3d7eab8d3853e9c76e9d2de
dingdong <1428837023@qq.com>
2023-07-14 15:00:42 +0800

    2.5.3 国际化处理

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/iBaiYang/go-gin-blog-eddycjy/docs"
+	"github.com/iBaiYang/go-gin-blog-eddycjy/internal/middleware"
 	v1 "github.com/iBaiYang/go-gin-blog-eddycjy/internal/routers/api/v1"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -12,6 +13,8 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	r.Use(middleware.Translations())
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
