3003a2d02d6fb3235cef742f8f091107d6d19a91
dingdong <1428837023@qq.com>
2023-07-19 22:16:07 +0800

    2.9.6 注册中间件

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -7,10 +7,19 @@ import (
 	"github.com/iBaiYang/go-gin-blog-eddycjy/internal/middleware"
 	"github.com/iBaiYang/go-gin-blog-eddycjy/internal/routers/api"
 	v1 "github.com/iBaiYang/go-gin-blog-eddycjy/internal/routers/api/v1"
+	"github.com/iBaiYang/go-gin-blog-eddycjy/pkg/limiter"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"net/http"
+	"time"
 )
+
+var methodLimiters = limiter.NewMethodLimiter().AddBuckets(limiter.LimiterBucketRule{
+	Key:          "/auth",
+	FillInterval: time.Second,
+	Capacity:     10,
+	Quantum:      10,
+})
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
@@ -18,11 +27,20 @@ func NewRouter() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
-	r.Use(middleware.Translations())
-
 	r.Use(middleware.AccessLog())
 
-	//r.Use(middleware.Recovery())
+	//if global.ServerSetting.RunMode == "debug" {
+	//	r.Use(gin.Logger())
+	//	r.Use(gin.Recovery())
+	//} else {
+	//	r.Use(middleware.AccessLog())
+	//	r.Use(middleware.Recovery())
+	//}
+
+	r.Use(middleware.RateLimiter(methodLimiters))
+	r.Use(middleware.ContextTimeout(60 * time.Second))
+
+	r.Use(middleware.Translations())
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
